c429e826b17416e7e22d8e4b832f59c2a9ca0cbf
Vladislav Vishyankov <84133235+TTMPower@users.noreply.github.com>
2021-07-30 15:37:33 +0600

    Initial Commit

---
+++ VideoSubtitles/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoSubtitles
+//
+//  Created by Владислав Вишняков on 30.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
