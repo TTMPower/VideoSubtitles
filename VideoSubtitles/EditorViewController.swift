650130107cd005822385ded9d37d00562c96f37c
Vladislav Vishyankov <84133235+TTMPower@users.noreply.github.com>
2021-08-02 18:07:25 +0600

    add duration time and play/pause button

---
+++ VideoSubtitles/EditorViewController.swift
@@ -0,0 +1,88 @@
+//
+//  EditorViewController.swift
+//  VideoSubtitles
+//
+//  Created by Владислав Вишняков on 02.08.2021.
+//
+
+import UIKit
+import AVFoundation
+
+class EditorViewController: UIViewController {
+    
+    var index = 0
+    var time = Timer()
+    
+    func timers() {
+        time = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
+            self.playOrPauseOutletButton.isHidden = true
+    }
+    }
+    @IBOutlet weak var endTimeLabel: UILabel!
+    
+    @IBOutlet weak var playOrPauseOutletButton: UIButton!
+    @IBOutlet weak var playerViewOutlet: UIView!
+    @IBAction func playOrPauseAction(_ sender: Any) {
+        timers()
+        if index == 0 {
+            play()
+        } else if index == 1 {
+            stop()
+        }
+    }
+    var urlVideo: URL? = nil
+    var player: AVPlayer!
+    var playerLayer: AVPlayerLayer!
+    
+    
+    func play() {
+        playOrPauseOutletButton.setImage(UIImage(named: "pause"), for: .normal)
+        player.play()
+        index = 1
+    }
+    
+    func stop() {
+        playOrPauseOutletButton.setImage(UIImage(named: "play-button"), for: .normal)
+        player.pause()
+        index = 0
+    }
+    
+    @objc func handleTap(_ sender: UITapGestureRecognizer? = nil) {
+        time.invalidate()
+        if playOrPauseOutletButton.isHidden == true {
+            playOrPauseOutletButton.isHidden = false
+            time.invalidate()
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let tap = UITapGestureRecognizer(target: self, action: #selector(self.handleTap(_:)))
+        playerViewOutlet.addGestureRecognizer(tap)
+        playerViewOutlet.isUserInteractionEnabled = true
+        let asset = AVURLAsset(url: urlVideo!)
+        let totalSeconds = Int(CMTimeGetSeconds(asset.duration))
+        let minutes = totalSeconds / 60
+        let seconds = totalSeconds % 60
+        let mediaDuration = String(format:"%02i:%02i",minutes, seconds)
+        endTimeLabel.text = mediaDuration
+//        playOrPauseOutletButton.imageView?.image =
+        if let unwrapURL = urlVideo {
+            player = AVPlayer(url: unwrapURL)
+            playerLayer = AVPlayerLayer(player: player)
+            playerLayer.videoGravity = .resizeAspect
+            playerViewOutlet.layer.addSublayer(playerLayer)
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        player.pause()
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        playerLayer.frame = playerViewOutlet.bounds
+    }
+}
--- VideoSubtitles/PickerViewController.swift
+++ VideoSubtitles/PickerViewController.swift
@@ -8,9 +8,11 @@
 import UIKit
 import AVFoundation
 
-class PickerViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+class PickerViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     let picker = UIImagePickerController()
+    
+    @IBOutlet weak var labelText: UILabel!
     
     @IBAction func pickerVideo(_ sender: Any) {
         picker.delegate = self
@@ -21,20 +23,27 @@ class PickerViewController: UIViewController, UIImagePickerControllerDelegate & 
     }
     // MARK: Делегат отмены выбора видео
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-            dismiss(animated: true, completion: nil)
+        dismiss(animated: true, completion: nil)
         print("Видео не выбрано")
     }
     
     // MARK: Делегат выброна метода
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-            dismiss(animated: true, completion: nil)
-            guard let movieUrl = info[.mediaURL] as? URL else {
-                return
-            }
-        // Работа с видео
-            print(movieUrl)
+        dismiss(animated: true, completion: nil)
+        guard let movieUrl = info[.mediaURL] as? URL else {
+            return
+        }
+        transferURL(url: movieUrl)
     }
+    
+    func transferURL(url: URL) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        guard let secondViewController = storyboard.instantiateViewController(identifier: "SecondViewController") as? EditorViewController else { return }
+        secondViewController.urlVideo = url
+        present(secondViewController, animated: true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         picker.delegate = self
